3e67e8d762d79b909dcb4745923833e681fca31b
MidoryFFF <127252583+MidoryFFF@users.noreply.github.com>
2025-12-02 23:21:43 +0300

    Code clearing

--- main.py
+++ main.py
@@ -70,8 +70,8 @@ def Write(adress, value):
 
 def BSWP():
     global acamulator
-    #print(bin(acamulator)[6:] + bin(acamulator)[:5], int(bin(acamulator)[6:] + bin(acamulator)[:5], 2))
-    acamulator = int(bin(acamulator)[6:] + bin(acamulator)[:5], 2)
+    #print(bin(acamulator), bin(acamulator)[6:] + bin(acamulator)[2:5], int(bin(acamulator)[6:] + bin(acamulator)[2:5], 2))
+    acamulator = int(bin(acamulator)[6:] + bin(acamulator)[2:5], 2)
 
 def WriteInFile():
     with open(argsSet["FilePath"], "w") as f:
@@ -86,35 +86,53 @@ def ReadProgram():
     with open(argsSet["ProgrammPath"]) as f:
         ParsProgramm(f.read())
 
+def MinorPars(argsPars: str, nameAction: str):
+    temp = [nameAction[:-3], [j[:-2].replace(" ", "") if j[-1] in ["{", "}"] else j.replace(" ", "") for j in argsPars.split(", ")]]
+    # temp = []
+    # for j in argsPars.split(", "):
+    #     if j[-1] in ["{", "}", " "]:
+    #         temp.append([nameAction[:-3], [j[:-2].replace(" ", "")]])
+    #     else:
+    #         j.replace(" ", "")
+    # temp = temp[0]
+    for a in range(len(temp[1])):
+        if (":" in temp[1][a]):
+            temp[1][a] = temp[1][a].split(":")
+            temp[1][a][1] = int(temp[1][a][1])
+        print(temp[0], temp[1][a], temp[1])
+    return temp
+
 def ParsProgramm(ProgramString: str):
     prog = ProgramString.split("\n")
     func = prog[0]
     buf = ""
     for i in range(1, len(prog)):
         if not prog[i][0] in [" ", "\t"]:
-            application.append([func[:-3], [j[:-2].replace(" ", "") if j[-1] in ["{", "}", " "] else j.replace(" ", "") for j in buf.split(", ")]])
+            application.append(MinorPars(buf, func))
             func = prog[i]
             buf = ""
         else:
             buf += prog[i]
-    application.append([func[:-3], [j[:-2].replace(" ", "") if j[-1] in ["{", "}"] else j.replace(" ", "") for j in buf.split(", ")]])
-    [print(i) for i in application]
+    application.append(MinorPars(buf, func))
+    #[print(i) for i in application]
     return application
 
 def RunProgam():
     for i in application:
+        print(i[0], i[1])
         if i[0] == "const":
             buf = dict()
             for a in i[1]:
-                a = a.split(":")
-                buf[a[0]] = int(a[1])
+                buf[a[0]] = a[1]
             Write(buf["adress"], buf["value"])
         elif i[0] == "read":
-            Read(int(i[1][0].split(":")[1]))
+            Read(i[1][0][1])
         elif i[0] == "write":
-            Write(int(i[1][0].split(":")[1]), acamulator)
+            Write(i[1][0][1], acamulator)
         elif i[0] == "bswap":
             BSWP()
+        else:
+            print("Error: unkown command")
 
 def TestRunMode():
     ReadProgram()
